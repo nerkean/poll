385d7e21ef6f724633ae510eb29e4d9266c57468
nerkean <147550511+nerkean@users.noreply.github.com>
2026-02-20 12:31:05 +0200

    Fix import statement for GoogleGenerativeAI

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const { GoogleGenerativeAI } = require(@google/generative-ai)
+const { GoogleGenerativeAI } = require('@google/generative-ai')
 const Response = require('./models/Response');
 
 const app = express();
